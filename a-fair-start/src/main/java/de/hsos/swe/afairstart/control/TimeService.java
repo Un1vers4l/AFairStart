f8bf1177cfcf21a0233389c5ee0f624e1f37520c
Mario <megaman_2@live.de>
2022-05-30 19:28:14 +0200

    Grundgerüst erstellt

---
+++ a-fair-start/src/main/java/de/hsos/swe/afairstart/control/TimeService.java
@@ -0,0 +1,5 @@
+package de.hsos.swe.afairstart.control;
+
+public interface TimeService {
+    int getTime (User user, Device devic, int processTime);
+}
---
+++ a-fair-start/src/main/java/de/hsos/swe/afairstart/gateway/repositories/UserRepos.java
@@ -0,0 +1,5 @@
+package de.hsos.swe.afairstart.gateway.repositories;
+
+public class UserRepos {
+    
+}
